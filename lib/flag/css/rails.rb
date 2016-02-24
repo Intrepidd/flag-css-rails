17b866f0ad886448a267768b50504c6c918ade8f
Adrien Siami <adrien.siami@homestay.com>
2016-02-24 17:44:44 +0000

    Initial commit

---
+++ lib/flag/css/rails.rb
@@ -0,0 +1,10 @@
+require "flag/css/rails/version"
+
+module Flag
+  module Css
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
---
+++ lib/flag/css/rails/version.rb
@@ -0,0 +1,7 @@
+module Flag
+  module Css
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
